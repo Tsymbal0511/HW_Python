608c8a77124bbec85de20df9a3f62232617ba2e1
Serhii Tsymbal <tsymbal0511@gmail.com>
2023-03-09 14:42:20 +0300

    finish hw

--- HW_5/home_work_5.py
+++ HW_5/home_work_5.py
@@ -2,8 +2,16 @@
 #*Пример:*
 #A = 3; B = 5 -> 243 (3⁵)
 #A = 2; B = 3 -> 8 
+'''
+def recAB(a, b):
+    if b == 0:
+        return 1
+    return a * recAB(a, b - 1)
 
-
+a = int(input('Введите число: '))
+b = int(input('Введите степень: '))
+print(recAB(a, b))
+'''
 
 
 #Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. 
@@ -11,3 +19,9 @@
 #*Пример:*
 #2 2 4 
 
+def recsum(a, b):
+    if b == 0:
+        return a
+    return 1 + recsum(a, b - 1)
+
+print(recsum(6, 4))
